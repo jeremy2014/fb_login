914e30a6f47673baf90c6969fa209dbe810b6b5f
Jeremiah Sebastian Samatra <jeremiah_cool01@yahoo.com>
2014-01-06 15:43:08 +0800

    Facebook login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FbLogin::Application.config.secret_key_base = '208deca02d1fe6f04e338dd8130fe01456eb5be9323a59fc72cf8f8e110fe5eafb6e32d2ba5a7bca7241bc0751fe16290404438233df6861def139d1452f81b9'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+FbLogin::Application.routes.draw do
+  devise_for :users
+  root to: "home#index"
+end
